28d3ca9deb390c0ebdb138dfd6d5beaf392e9cb1
vishnumohanan01 <vishnu.mohanan@bigbinary.com>
2022-08-12 12:12:49 +0530

    added alias modules - general and git

---
+++ git_aliases.sh
@@ -0,0 +1,6 @@
+alias ga="git add";
+alias gci="git commit -m";
+alias gst="git status";
+alias gco="git checkout";
+alias gb="git branch";
+alias gbD="git branch -D";
--- import_all.sh
+++ import_all.sh
@@ -1,2 +1,5 @@
-source general_aliases.sh
-source git_aliases.sh
+# Sets all my aliases by importing alias modules
+
+WORKING_DIR="$HOME/.all-my-bash-aliases";
+source "$WORKING_DIR/general_aliases.sh";
+source "$WORKING_DIR/git_aliases.sh";
