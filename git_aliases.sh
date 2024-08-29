dc227db009dc49ca7c52d0abd5384be1b2f871ab
vishnuMohanan01 <iamvm.dev@gmail.com>
2024-08-29 23:45:57 +0530

    Add gpo git alias

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,8 +1,9 @@
-alias ga="git add";
-alias gci="git commit -m";
-alias gst="git status";
-alias gco="git checkout";
-alias gb="git branch";
-alias gbD="git branch -D";
-alias gpl="git pull";
-alias gpu="git push";
+alias ga="git add"
+alias gci="git commit -m"
+alias gst="git status"
+alias gco="git checkout"
+alias gb="git branch"
+alias gbD="git branch -D"
+alias gpl="git pull"
+alias gpu="git push"
+alias gpo="git push origin"
